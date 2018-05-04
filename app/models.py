76784d31c253bb1e602fa4a0ed78331224eb5587
hugodias360 <hugodias360@gmail.com>
2018-05-04 20:33:01 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -12,4 +12,11 @@ class Curso(models.Model):
 
 class Vestibular(models.Model):
     nome = models.CharField(max_length=200)
+    
+class Curso(models.Model):
+    nome = models.CharField(max_length=200)
+    RG = models.CharField(max_length=200)
+    CPF = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=520)
+    telefone = models.CharField(max_length=200)
 
